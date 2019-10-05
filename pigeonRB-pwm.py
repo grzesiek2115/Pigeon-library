4887b3819cc5807569a7497e9b95b5429789aae2
grzesiek2115 <55406786+grzesiek2115@users.noreply.github.com>
2019-10-06 00:40:47 +0200

    pwm

---
+++ pigeonRB-pwm.py
@@ -0,0 +1,21 @@
+from pigeonRB import *
+
+pigeonSetup(Model.RB300)
+enableOutputs(True)
+
+out7 = GPIO.PWM(OUT7, 50)    #GPIO.PWM(channel, frequency)
+wypelnienie = 0
+out7.start(wypelnienie)              #.start(wypełnienie 0 - 100)
+
+try:
+    while True:
+        wypelnienie += 5
+        if  wypelnienie >= 100:
+            wypelnienie = 0
+        out7.ChangeDutyCycle(wypelnienie)   #ustawienie nowej wartości wypełnienia
+
+except KeyboardInterrupt:
+    print("Koniec")
+
+out7.stop()
+GPIO.cleanup
