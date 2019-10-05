3861c99b4989c22aa38e45d6820d5492b77cab5e
grzesiek2115 <55406786+grzesiek2115@users.noreply.github.com>
2019-10-05 23:59:06 +0200

    adding new file2

---
+++ pigeonRB-inputs.py
@@ -0,0 +1,23 @@
+from pigeonRB import *
+
+pigeonSetup(Model.RB300)
+
+print("Optoisolated inputs: \n\r")
+print("IN1 = {}".format(1 - readOptoInput(1)))
+print("IN1 = {}".format(1 - readOptoInput(2)))
+print("IN1 = {}".format(1 - readOptoInput(3)))
+print("IN1 = {}".format(1 - readOptoInput(4)))
+print("IN1 = {}".format(1 - readOptoInput(5)))
+print("IN1 = {}".format(1 - readOptoInput(6)))
+print("IN1 = {}".format(1 - readOptoInput(7)))
+print("IN1 = {}".format(1 - readOptoInput(8)))
+print("----------------------\n\r")
+print("Dryisolated inputs: \n\r")
+print("IND1 = {}".format(1 - readDryInput(1)))
+print("IND2 = {}".format(1 - readDryInput(2)))
+print("IND3 = {}".format(1 - readDryInput(3)))
+print("IND4 = {}".format(1 - readDryInput(4)))
+
+exit()
+
+
