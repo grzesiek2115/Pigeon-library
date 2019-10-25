98e54ada44622f58c4666ea3960c7410e4c69324
grzesiek2115 <55406786+grzesiek2115@users.noreply.github.com>
2019-10-25 19:58:28 +0200

    Analog interfaces

---
+++ pigeonSPI.py
@@ -0,0 +1,53 @@
+import spidev
+import math
+import time
+
+def spiClose():
+    spi.close()
+
+
+def spiWriteAO(val):
+    spi = spidev.SpiDev()  # create spi object
+    spi.open(0, 1)  # open spi port 0, device (CS) 1
+    spi.mode = 1  # mode bits: CPHA = 1, CPOL = 0,
+    spi.bits_per_word = 8
+    spi.max_speed_hz = 2000000
+    a = (val[0] % 256)
+    b = (val[0] / 256)
+    c = (val[1] % 256)
+    d = (val[1] / 256)
+    bb = math.floor(b)
+    dd = math.floor(d)
+    spi.writebytes([0x4F, a, bb, c, dd])
+
+
+def spiReadAI():
+    spi = spidev.SpiDev()  # create spi object
+    spi.open(0, 1)  # open spi port 0, device (CS) 1
+    spi.mode = 1
+    spi.bits_per_word = 8
+    spi.max_speed_hz = 2000000
+
+    spi.writebytes([0x41])
+    time.sleep(0.0001)
+    q = spi.readbytes(8)
+
+    x = []
+
+    x.append((q[0]) | (q[1] << 8))
+    x.append((q[2]) | (q[3] << 8))
+    x.append((q[4]) | (q[5] << 8))
+    x.append((q[6]) | (q[7] << 8))
+
+    a = (x[0] * 10.065) / 1023.0
+    b = (x[1] * 10.065) / 1023.0
+    c = (x[2] * 10.065) / 1023.0
+    d = (x[3] * 10.065) / 1023.0
+
+    print(round(a,2), round(b,2), round(c,2), round(d,2))
+
+#def spiReadRegs():
+
+#def spiWriteRegs():
+
+#def spiReadFirmwareVer():
